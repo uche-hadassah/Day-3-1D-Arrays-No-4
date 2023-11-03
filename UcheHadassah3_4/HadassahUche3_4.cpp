8c6d342aebda6cebe5b3233530904faab9573344
Hadassah Uche <uhadassah1@gmail.com>
2023-11-03 15:19:43 +0200

    Tried free hand coding. Not bad but there are quite a few logical bugs

--- UcheHadassah3_4/HadassahUche3_4.cpp
+++ UcheHadassah3_4/HadassahUche3_4.cpp
@@ -8,6 +8,23 @@ int main()
 	double Array[MAX];
 	double sum = 0.0;
 	double Average;
-
+	int counter = 0;
+	cout << "Enter a number(-1 to end):";
+	do
+	{
+		cin >> Array[counter];
+		if (Array[counter] < -1)
+		{
+			cout << "Invalid number. Enter a POSITIVE number:";
+			cin >> Array[counter];
+		}
+		else if (Array[counter] == -1)
+		{
+			break;
+		}
+		sum += Array[counter];
+	} while (Array[counter] != -1 && Array[counter] > -1);
+	Average = sum / counter;
+	cout << "The average of the numbers you entered is:" << Average;
 	return 0;
 }
