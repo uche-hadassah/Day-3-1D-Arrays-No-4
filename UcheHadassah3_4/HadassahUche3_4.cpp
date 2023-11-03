0687d1598fd685c738daa676b3c911f81afdc3ca
Hadassah Uche <uhadassah1@gmail.com>
2023-11-03 15:09:46 +0200

    Created the skeleton of this program

---
+++ UcheHadassah3_4/HadassahUche3_4.cpp
@@ -0,0 +1,13 @@
+/*Calculating Average: Write a program that reads a sequence of positive doubles into 
+an array (-1 to finish) and outputs the array that was read along with the average*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	const int MAX = 100;
+	double Array[MAX];
+	double sum = 0.0;
+	double Average;
+
+	return 0;
+}
